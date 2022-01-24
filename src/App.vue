b4636daec93adcd254cf5ea9845f3994b1630274
AngeloDalo <angelo251158@gmail.com>
2022-01-24 11:26:44 +0100

    variables

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+      <Header />
+      <Main />
+      <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Main from "./components/Main.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
+    Main,
+    Footer,
   },
 };
 </script>
@@ -19,11 +24,8 @@ export default {
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap"; App.vue
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Libre Baskerville', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer>
+        <footer>footer</footer>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/partials/_variables.scss";
+@import "../assets/scss/partials/_commons.scss";
+</style>
