a32769d1d2b6afeeee43bacee6429ea7bd765ba1
AngeloDalo <angelo251158@gmail.com>
2022-01-24 16:47:14 +0100

    MainCondition

--- src/components/MainCondition.vue
+++ src/components/MainCondition.vue
@@ -1,10 +1,9 @@
 <template>
-    <li class="col-3">
+    <li class="col-3 d-flex flex-column align-items-center">
+        <div><i class="icons" :class="icons"></i></div>
         <h4>{{ title }}</h4>
         <img :src="img" :alt="alt">
         <p>{{ text }}</p>
-        <i class="fas fa-heart"></i>
-        <i class="bi bi-123"></i>
     </li>
 </template>
 
@@ -24,17 +23,33 @@ export default {
 @import "../assets/scss/partials/_variables.scss";
 @import "../assets/scss/partials/_commons.scss";
 li {
-    text-align: center;
-    i {
-        color: red;
+    div {
+        margin-bottom: 1em;
+        border:1px solid red;
+        border-radius: 100%;
+        width: 50px;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .icons {
+            color: red;
+            font-size: 1.5em;
+        }
     }
+    text-align: center;
     h4 {
+        margin-bottom: 1em;
         font-size: 1em;
         color: white;
         font-weight: 700;
     }
     p {
+        margin-top: 1em;
         color: $TextColorSummary;
     }
+}
+i {
+    font-size: 2em;
 }
 </style>
